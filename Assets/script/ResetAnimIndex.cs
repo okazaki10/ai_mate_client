716232ccde630c1992d35586919e575442d8b7b5
okazaki10 <rakaabadisusilo@gmail.com>
2025-07-05 09:55:15 +0700

    fix animation

---
+++ Assets/script/ResetAnimIndex.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class ResetAnimIndex : StateMachineBehaviour
+{
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.SetInteger("animBaseInt", 0);
+    }
+}
